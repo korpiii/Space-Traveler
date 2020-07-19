64c6af75284abe69ade8f3f355439c6ab94514f0
Dian Delph <da.dnvl@live.com>
2020-07-19 16:30:50 -0500

    Adds player movement, background endless navigation and parallax

---
+++ Assets/StarfieldOffset.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StarfieldOffset : MonoBehaviour
+{
+    public float parallax = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MeshRenderer mr = GetComponent<MeshRenderer>();
+        Material mat = mr.material;
+        Vector2 offset = mat.mainTextureOffset;
+        offset.x = transform.position.x / transform.localScale.x /parallax;
+        offset.y = transform.position.y / transform.localScale.y / parallax;
+        mat.mainTextureOffset = offset;
+
+    }
+}
